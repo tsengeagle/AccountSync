18230e1b9d78a98f435f2b6807bf6f94a18c4d8d
tsengeagle <tsengeagle@gmail.com>
2016-02-02 10:40:46 +0800

    新增簡易登入功能 只有Authentication 還沒有Authorization

--- AccountSync/Controllers/HomeController.cs
+++ AccountSync/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace AccountSync.Controllers
 {
@@ -12,6 +13,27 @@ namespace AccountSync.Controllers
         {
             return RedirectToAction("Query", "ProxyAccount");
             //return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            Models.LoginViewModel model = new Models.LoginViewModel() { ReturnURL = returnUrl };
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(Models.LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            FormsAuthentication.RedirectFromLoginPage(model.UserID, false);
+
+            return View();
         }
     }
 }

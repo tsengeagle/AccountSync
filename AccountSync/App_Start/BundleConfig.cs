024fafc24a5eaf541c2a0b5504d7ea4746df9518
tsengeagle <tsengeagle@gmail.com>
2016-02-02 08:44:41 +0800

    新增bootstrap-datepicker

--- AccountSync/App_Start/BundleConfig.cs
+++ AccountSync/App_Start/BundleConfig.cs
@@ -21,10 +21,14 @@ namespace AccountSync
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap-datepicker.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-datepicker.css",
+                      "~/Content/bootstrap-datepicker3.css",
+                      "~/Content/PagedList.css",
                       "~/Content/site.css"));
         }
     }
